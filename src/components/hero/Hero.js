57391db659a08febef7617630dc3fb1def4d0827
Ryan <ryanevans253@yahoo.com>
2022-02-24 12:39:16 -0800

    hero background fixed

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,9 +1,13 @@
 import Menu1 from "../menu1/Menu1";
 import Menu2 from "../menu2/Menu2";
+import video from "./nature.mp4";
 
 export default function Hero() {
   return (
     <div>
+      <video muted loop autoPlay className="heroVideo">
+        <source src={video} type="video/mp4" />
+      </video>
       <Menu1 />
       <Menu2 />
     </div>
