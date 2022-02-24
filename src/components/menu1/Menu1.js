e6606c9ab5d24bd383b72fa7204242dd5c1221df
Ryan <ryanevans253@yahoo.com>
2022-02-24 13:43:14 -0800

    finished top menu

--- src/components/menu1/Menu1.js
+++ src/components/menu1/Menu1.js
@@ -1,10 +1,17 @@
 import Button from "../button/Button";
+// import img from "./search.svg";
+import { ReactComponent as Icon } from "./search.svg";
 
 export default function Menu1() {
   return (
     <div className="topMenu">
       <h1>slalom</h1>
-      <Button text="let's talk" />
+      <div className="right">
+        <Button text="let's talk" />
+        {/* <img className="icon" src={img} /> */}
+        <Icon className="icon" fill="white" stroke="white" />
+        <p>english</p>
+      </div>
     </div>
   );
 }
